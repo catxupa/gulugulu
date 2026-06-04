5aabd93328ebc3bded5f533aa2ce8319d4660a0e
rafa <monteir20@icloud.com>
2026-06-04 10:02:05 -0100

    gulu

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,12 +1,12 @@
 "use client";
 
-import { ApolloProvider } from "@apollo/client/react";
+
 import client from "@/services/apollo";
 
 type ProvidersProps = {
   children: React.ReactNode;
 };
 
-export function Providers({ children }: ProvidersProps) {
-  return <ApolloProvider client={client}>{children}</ApolloProvider>;
+export function ApolloClientProviders({ children }: ProvidersProps) {
+  return <ApolloClientProviders>{children}</ApolloClientProviders>;
 }
